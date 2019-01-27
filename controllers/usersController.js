517e5a3647b66e30ae5aa0be04ad57a4bbc5b723
Jacob Mink <jacobmink@gmail.com>
2019-01-26 17:18:24 -0700

    Started usersController.js

---
+++ controllers/usersController.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users');
+
+
+
+
+// ROUTES
+
+router.route('/')
+    // index
+    .get(async (req,res)=>{
+        try{
+            const allUsers = await User.find({});
+            res.render('/users/index.ejs', {
+                users: allUsers
+            });
+        }catch(err){
+            res.send(err);
+        }
+    })
+    // post
+    .post((req,res)=>{
+        try{
+            await User.create(req.body);
+            res.redirect('/users');
+        }catch(err){
+            res.send(err);
+        }
+    });
+
+router.route('/new')
+    .get((req,res)=>{
+            res.render('/users/new.ejs');
+    });
+
+router.router('/:id')
+    // show
+    .get(async (req,res)=>{
+        try{
+            const foundUser = User.findById(req.params.id);
+            res.render('/users/show.ejs', {
+                user: foundUser
+            });
+        }catch(err){
+            res.send(err);
+        }
+    })
+    // update
+    .put(async (req,res)=>{
+        try{
+            const foundUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true});
+            res.redirect(`/users/${req.params.id}`);
+        }catch(err){
+            res.send(err);
+        }
+    })
+    // delete
+    .delete(async (req,res)=>{
+        try{
+            const deletedUser = await User.findByIdAndDelete(req.params.id);
+            res.redirect('/');
+        }catch(err){
+            res.send(err);
+        }
+    })
+
+router.router('/:id/edit')
+    .get(async (req,res)=>{
+        try{
+            const foundUser = await User.findById(req.params.id);
+            res.render('/users/edit.ejs', {
+                user: foundUser
+            });
+        }catch(err){
+            res.send(err);
+        }
+    })
