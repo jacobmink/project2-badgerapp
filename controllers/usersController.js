7f8e09308389cc7587a651ff47cb263bdb9098b2
Jacob Mink <jacobmink@gmail.com>
2019-01-27 20:32:08 -0700

    Added login route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 const User = require('../models/users');
-
+const Badge = require('../models/badges');
 
 
 
@@ -38,11 +38,14 @@ router.route('/')
 //             res.render('/users/new.ejs');
 //     });
 
+
+
+
 router.route('/:id')
-    // show
+    // show profile
     .get(async (req,res)=>{
         try{
-            const foundUser = User.findById(req.params.id);
+            const foundUser = await User.findById(req.params.id);
             res.render('/users/show.ejs', {
                 user: foundUser
             });
@@ -50,25 +53,39 @@ router.route('/:id')
             res.send(err);
         }
     })
-    // update
+    // update profile
     .put(async (req,res)=>{
         try{
-            const foundUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true});
+            await User.findByIdAndUpdate(req.params.id, req.body, {new: true});
             res.redirect(`/users/${req.params.id}`);
         }catch(err){
             res.send(err);
         }
     })
-    // delete
+    // delete account
     .delete(async (req,res)=>{
         try{
-            const deletedUser = await User.findByIdAndDelete(req.params.id);
+            await User.findByIdAndDelete(req.params.id);
             res.redirect('/');
         }catch(err){
             res.send(err);
         }
     })
 
+router.route('/:id/newbadge')
+    .get(async (req,res)=>{
+        try{
+            const foundUser = await User.findById(req.params.id);
+            res.render('/users/newBadge.ejs', {
+                badgeList: foundUser.badgeList
+            });
+        }catch(err){
+            res.send(err);
+        }
+    })
+    
+
+    // edit user profile
 router.route('/:id/edit')
     .get(async (req,res)=>{
         try{
