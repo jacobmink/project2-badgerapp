e617d1c2fb8c57aa06fc113b125ac1f12d9aca2f
Jacob Mink <jacobmink@gmail.com>
2019-01-30 14:15:22 -0700

    Add filters working

--- controllers/badgesController.js
+++ controllers/badgesController.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const User = require('../models/users');
 const Badge = require('../models/badges');
 
+const badgeTitles = [
+    'hike',
+    'bike',
+    'swim',
+    'cook'
+];
 
 router.route('/')
     .get(async (req,res)=>{
@@ -17,12 +23,7 @@ router.route('/')
     })
 
 
-const badgeTitles = [
-    'hike',
-    'bike',
-    'swim',
-    'cook'
-];
+
 
 router.route('/new')
     .get(async (req,res)=>{
@@ -35,6 +36,7 @@ router.route('/new')
                 badgeTitles: badgeTitles
             })
         }catch(err){
+            console.log(err);
             res.send(err);
         }
     })
@@ -42,9 +44,11 @@ router.route('/new')
 router.route('/:id')
     .get(async (req,res)=>{
         try{
+            const foundUser = await User.findById(req.userId);
             const foundBadge = await Badge.findById(req.params.id);
-            res.render('/badges/show.ejs', {
-                badge: foundBadge
+            res.render('badges/show.ejs', {
+                badge: foundBadge,
+                user: foundUser
             });
         }catch(err){
             res.send(err);
