cbc55abdaff47e2a32098a33b36a886e722089ff
Jacob Mink <jacobmink@gmail.com>
2019-01-31 22:45:50 -0700

    filter toggle

---
+++ public/js/filtertoggle.js
@@ -0,0 +1,8 @@
+$('.filter-button').click((e)=>{
+    // if($(e.currentTarget).html('Show Filters')){
+    //     $(e.currentTarget).html('Hide Filters');
+    // }if($(e.currentTarget).html('Hide Filters')){
+    //     $(e.currentTarget).html('Show Filters');
+    // }
+    $('.filter-form').toggle();
+})
